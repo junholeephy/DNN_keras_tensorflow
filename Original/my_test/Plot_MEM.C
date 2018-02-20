53a844053a64ab05ffaf6d5e787df731fb340b85
junholeephy <skyblue1293@naver.com>
2018-02-20 12:49:34 +0100

    plotting

--- Original/my_test/Plot_MEM.C
+++ Original/my_test/Plot_MEM.C
@@ -4,16 +4,13 @@
 #include "TFile.h"
 #include "root_tree_reader.h"
 #include "branch_from_tree.h"
-//#include "read_branch_name.h"
-//#include "branch_to_histo.h"
 
 void Plot_MEM()
 {
 //    string str = "../../self_trees/self_tree_mem_EN90000_LN1_E5000_NN50_B500_adam_LR1e-05_DR0.0.root"; 
-//    string str = "../../input_TTZ_DelphesEvalGen_5275k.root";
     string str = "generative_tree_1D.root";
     int NBIN = 100;
-    double LOW = -10;
+    double LOW = -15;
     double HIGH = 2;
 
     root_tree_reader *RTR = new root_tree_reader(&str);
@@ -21,10 +18,10 @@ void Plot_MEM()
     branch_from_tree *BFT = new branch_from_tree();
     vector<TBranch*> vbranch;
 
-    TH1F* HistoOutput_Train = new TH1F("HistoOutput_Train","HistoOutput_Train",NBIN, LOW, HIGH);
-    TH1F* HistoOutput_Test  = new TH1F("HistoOutput_Test","HistoOutput_Test",NBIN, LOW, HIGH);
-    TH1F* HistoTarget_Train = new TH1F("HistoTarget_Train","HistoTarget_Train",NBIN, LOW, HIGH);
-    TH1F* HistoTarget_Test  = new TH1F("HistoTarget_Test","HistoTarget_Test",NBIN, LOW, HIGH);
+    TH1F* HistoOutput_Train = new TH1F("HistoOutput_Train","HistoOutput_Train",NBIN, LOW, HIGH);  HistoOutput_Train->SetMarkerSize(0.6);  HistoOutput_Train->SetMarkerStyle(kFullCircle); HistoOutput_Train->SetMarkerColor(kBlue);
+    TH1F* HistoOutput_Test  = new TH1F("HistoOutput_Test","HistoOutput_Test",NBIN, LOW, HIGH);    HistoOutput_Test->SetMarkerSize(0.6);  HistoOutput_Test->SetMarkerStyle(kFullCircle); HistoOutput_Test->SetMarkerColor(kRed);
+    TH1F* HistoTarget_Train = new TH1F("HistoTarget_Train","HistoTarget_Train",NBIN, LOW, HIGH);  HistoTarget_Train->SetLineColor(kBlue);
+    TH1F* HistoTarget_Test  = new TH1F("HistoTarget_Test","HistoTarget_Test",NBIN, LOW, HIGH);    HistoTarget_Test->SetLineColor(kRed);
     double target_train = 0;
     double output_train = 0;
     double target_test = 0;
@@ -67,6 +64,30 @@ void Plot_MEM()
     }
 	Double_t norm = 1.0;
     double scale = 1.0;
-//	HistoOutput_Train->Draw();
-//	HistoTarget_Train->Draw("same");
+    scale = norm/(HistoOutput_Train->Integral());
+    HistoOutput_Train->Scale(scale);
+    scale = norm/(HistoOutput_Test->Integral());
+    HistoOutput_Test->Scale(scale);
+    scale = norm/(HistoTarget_Train->Integral());
+    HistoTarget_Train->Scale(scale);
+    scale = norm/(HistoTarget_Test->Integral());
+    HistoTarget_Test->Scale(scale);
+
+    HistoOutput_Train->Draw("ep");    HistoOutput_Train->SetStats(0);
+    HistoOutput_Test->Draw("same ep");
+	HistoTarget_Train->Draw("HIST same");
+	HistoTarget_Test->Draw("HIST same");
+//    HistoOutput_Train->Draw("same ep");
+//    HistoOutput_Test->Draw("same ep");
+
+  TLegend* legend = new TLegend(0.7, 0.7, 0.9, 0.9, "");
+  legend->SetFillColor(kWhite);
+  legend->AddEntry(HistoTarget_Train->GetName(), "Target, train", "l");
+  legend->AddEntry(HistoOutput_Train->GetName(), "Output, train", "p");
+  legend->AddEntry(HistoTarget_Test->GetName(), "Target, test", "l");
+  legend->AddEntry(HistoOutput_Test->GetName(), "Output, test", "p");
+  legend->Draw();
+
+
+
 }
